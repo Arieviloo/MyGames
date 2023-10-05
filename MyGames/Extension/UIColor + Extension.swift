77e50c38e2b8c621ba5abeb7e648f294b7c51d06
Jadiê Oliveira <jadieoliveira00@gmail.com>
2023-10-05 15:42:46 -0300

    PlatformsViewController

--- MyGames/Extension/UIColor + Extension.swift
+++ MyGames/Extension/UIColor + Extension.swift
@@ -10,4 +10,5 @@ import UIKit
 extension UIColor {
     static let appPrimaryColor = UIColor(red: 88/255, green: 11/255, blue: 248/255, alpha: 1.0)
     static let appSecundaryColor = UIColor(red: 255/255, green: 246/255, blue: 238/255, alpha: 1.0)
+    static let appThirdColor = UIColor(red: 252/255, green: 80/255, blue: 151/255, alpha: 1.0)
 }
