f706fb69a1e983f52b546584f49408aa1a20a575
Jadiê Oliveira <jadieoliveira00@gmail.com>
2023-10-05 10:45:31 -0300

    init

---
+++ Extension/UIColor + Extension.swift
@@ -0,0 +1,13 @@
+//
+//  UIColor + Extension.swift
+//  MyGames
+//
+//  Created by Jadië Oliveira on 05/10/23.
+//
+
+import UIKit
+
+extension UIColor {
+    static let appPrimaryColor = UIColor(red: 88/255, green: 11/255, blue: 248/255, alpha: 1.0)
+    static let appSecundaryColor = UIColor(red: 255/255, green: 246/255, blue: 238/255, alpha: 1.0)
+}
